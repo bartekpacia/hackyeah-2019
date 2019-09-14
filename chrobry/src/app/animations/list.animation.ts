80b132efa8bde27b5b7dbd07eba576e3d85a43e2
Paweł Lubczyński <lubczynskip@gmail.com>
2019-09-15 01:37:49 +0200

    animation fixed

--- chrobry/src/app/animations/list.animation.ts
+++ chrobry/src/app/animations/list.animation.ts
@@ -3,11 +3,6 @@ import { animate, AnimationTriggerMetadata, query, stagger, style, transition, t
 export const listAnimation: AnimationTriggerMetadata =
   trigger('listAnimation', [
     transition('* => *', [ // each time the binding value changes
-      query(':leave', [
-        stagger(100, [
-          animate('0.5s', style({ opacity: 0 }))
-        ])
-      ], { optional: true }),
       query(':enter', [
         style({ opacity: 0 }),
         stagger(300, [
--- chrobry/src/app/modules/progress/containers/progress/progress.component.ts
+++ chrobry/src/app/modules/progress/containers/progress/progress.component.ts
@@ -9,7 +9,83 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 export class ProgressComponent {
   region = 'poland';
 
+  currentRegionList = [];
+
+  regions = {
+    poland: [
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men2',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men2',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_women',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_women2',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_women2',
+      }
+    ],
+    mazowieckie: [
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men2',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men2',
+      },
+    ],
+    warsaw: [
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+      {
+        title: 'Name Surname',
+        icon: 'avatar_men',
+      },
+    ]
+  };
+
   selectRegion(region: string): void {
     this.region = region;
+    this.currentRegionList = this.regions[region];
   }
 }
