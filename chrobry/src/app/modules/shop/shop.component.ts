d6bc530cb5b9f1bb7ffab55a3f44db7bd8453787
Paweł Lubczyński <lubczynskip@gmail.com>
2019-09-15 00:54:35 +0200

    shop layout

--- chrobry/src/app/modules/shop/shop.component.ts
+++ chrobry/src/app/modules/shop/shop.component.ts
@@ -3,10 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+  styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent {
 
-
+  itemList = [
+    {
+      label: 'hamburger',
+      points: '2000',
+      image: 'hamburger'
+    },
+    {
+      label: 'fries',
+      points: '2000',
+      image: 'fries'
+    },
+    {
+      label: 'coffee',
+      points: '2000',
+      image: 'coffee'
+    },
+    {
+      label: 'chicken',
+      points: '2000',
+      image: 'chicken'
+    },
+  ];
 
 }
