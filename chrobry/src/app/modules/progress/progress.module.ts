242b7e04cff4d1f77dceb7da0854f2400e97633a
Paweł Lubczyński <lubczynskip@gmail.com>
2019-09-14 22:08:17 +0200

    added progressbar

--- chrobry/src/app/modules/progress/progress.module.ts
+++ chrobry/src/app/modules/progress/progress.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ProgressRoutingModule } from './progress-routing.module';
 import { ProgressComponent } from './containers/progress/progress.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     ProgressComponent,
   ],
   imports: [
+    SharedModule,
     CommonModule,
     ProgressRoutingModule,
     FontAwesomeModule
