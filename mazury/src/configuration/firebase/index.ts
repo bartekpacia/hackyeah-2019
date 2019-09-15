abf0c21a4a7be52a2ac0ba94146c609b2a1a5f12
kondricz <kondricz@outlook.com>
2019-09-15 08:24:10 +0200

    login endpoint added

--- mazury/src/configuration/firebase/index.ts
+++ mazury/src/configuration/firebase/index.ts
@@ -1,7 +1,17 @@
 const admin = require('firebase-admin');
 
+if (process.env.NODE_ENV !== "production") {
+    const devCredentials = require("./hackyeah-2019-firebase-adminsdk-3jrt0-bc4e98a7a1.json")
+    admin.initializeApp({
+        credential: admin.credential.cert(devCredentials),
+        databaseURL: "https://hackyeah-2019.firebaseio.com"
+    });
+}
 
-admin.initializeApp();
+else {
+    admin.initializeApp();  
+}
+
 
 const DB = admin.firestore();
 const AUTH = admin.auth();
--- mazury/src/services/game/game.service.ts
+++ mazury/src/services/game/game.service.ts
@@ -6,19 +6,54 @@ import firebase from "../../configuration/firebase/"
 
 import Hooks from "./game.hooks"
 
+const userLevelMapper = (points: number) => {
+  switch (true) {
+    case (points < 20):
+        return 1;
+    case (points < 50):
+        return 2;
+    case (points < 100):
+        return 3;
+    case (points < 120):
+        return 4;  
+    case (points < 200):
+        return 5;  
+    case (points < 500):
+        return 6;  
+    case (points < 1000):
+        return 7;
+      }
+}
+
 const handler = async (req: Request, res: Response, next: NextFunction) => {
+  let user;
   const questionRef = firebase.DB.collection('QUESTIONS').doc(req.body.questionId)
+  const userRef = firebase.DB.collection('USERS').doc(req.body.userId)
+  await userRef.get().then(usr => {
+    const userObj = usr.data()
+    if (!userObj) {
+      res.locals.err = "INVALID USERID"
+      return next()
+    }
+    user = userObj;
+    return;
+  })
   await questionRef.get().then(async doc => {
-    const question = doc.data()
-    if (!question) {
+    const questionObj = doc.data()
+    if (!questionObj) {
       res.locals.err = "QUESTION NOT FOUND"
       return next()
     }
-    const { acceptedAnswerId, incorrectAnswerSum = 0, correctAnswerSum = 0 } = question;
+    const { acceptedAnswerId, incorrectAnswerSum = 0, correctAnswerSum = 0 } = questionObj;
+    const { streak = 0, correctQuestionSum = 0, inCorrectQuestionSum = 0, pkpPoints = 0 } = user;
     if (acceptedAnswerId !== req.body.answerId) {
       await questionRef.set({
         incorrectAnswerSum: incorrectAnswerSum + 1,
         succesRate: correctAnswerSum / (correctAnswerSum + incorrectAnswerSum)
+      }, { merge: true })
+      await userRef.set({
+        inCorrectQuestionSum: inCorrectQuestionSum + 1,
+        streak: 0
       }, { merge: true })
       res.locals.result = { status: "INCORRECT ANSWER" }
       return next();
@@ -26,6 +61,12 @@ const handler = async (req: Request, res: Response, next: NextFunction) => {
     await questionRef.set({
       correctAnswerSum: correctAnswerSum + 1,
       succesRate: correctAnswerSum / (correctAnswerSum + incorrectAnswerSum)
+    }, { merge: true })
+    await userRef.set({
+      correctQuestionSum: correctQuestionSum + 1,
+      streak: streak + 1,
+      level: userLevelMapper(correctQuestionSum + 1),
+      pkpPoints: pkpPoints + 1 + streak * 0.5 + Math.floor(Math.random() * 5)
     }, { merge: true })
     res.locals.result = { status: "CORRECT ANSWER" }
     return next();
@@ -43,10 +84,26 @@ export default function (app: Application) {
     ...Hooks.after
   ])
 
-  app.post('/login', (req: Request, res: Response, next: NextFunction) => {
+  app.post('/login', async (req: Request, res: Response, next: NextFunction) => {
     if (!req.body.email || !req.body.password) {
       return res.status(403).send({ status: "NOT_ALLOWED" })
     }
-    return res.status(200).send({ status: "OK", userId: uuid.v1() })
+    let currentUser = {}
+    const userRef = firebase.DB.collection('USERS')
+    await userRef.get().then(allUsers => {
+      allUsers.forEach(user => {
+        const userData = user.data()
+        if (userData.email === req.body.email) {
+          currentUser = {
+            ...userData,
+            id: user.id
+          };
+        }
+      })
+    })
+    if (!Object.keys(currentUser).length) {
+      return res.status(404).send({ status: "USER_NOT_FOUND" })
+    }
+    return res.status(200).send({ status: "OK", currentUser })
   })
 }
