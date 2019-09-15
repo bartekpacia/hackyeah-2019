42224790297a5e78d4f31a4cf6caa13864bef837
Bence Szegvari <bence.szegvari@gmail.com>
2019-09-15 09:51:10 +0200

    feat:badges

--- chrobry/src/app/modules/shop/shop.module.ts
+++ chrobry/src/app/modules/shop/shop.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ShopComponent } from './shop.component';
+import { ShopComponent } from './containers/shop/shop.component';
 import { ShopRoutingModule } from './shop-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
--- chrobry/src/app/modules/trophies/trophies.module.ts
+++ chrobry/src/app/modules/trophies/trophies.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TrophiesComponent } from './containers/trophies/trophies.component';
 import { RoutinTrophiesgModule } from './trophies-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   imports: [
     CommonModule,
     RoutinTrophiesgModule,
     SharedModule,
+    FontAwesomeModule,
   ],
   declarations: [TrophiesComponent]
 })
